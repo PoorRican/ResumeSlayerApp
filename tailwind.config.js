bfb24313e13edb70c6c8e31eec13c62071d2b9a8
Josue Figueroa <poor.rican@pm.me>
2023-09-05 09:33:53 -0700

    Added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/*.{js,jsx,ts,tsx}",
+    "./src/components/*.{js,jsx,ts,tsx}",
+    './public/index.html',
+
+  ],
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
