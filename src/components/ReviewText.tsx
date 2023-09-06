89bc6893a7681dc68b8e28033fab2d99c8d64ca9
Josue Figueroa <poor.rican@pm.me>
2023-09-06 02:08:45 -0700

    Add styling to `ReviewText` component

--- src/components/ReviewText.tsx
+++ src/components/ReviewText.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import { ResumeFormData } from "./ResumeCard";
 import FormButton from "./FormButton";
+import MarkdownBlock from "./MarkdownBlock";
 
 
 const ReviewText: React.FC<ResumeFormData & { handleSubmitForm: () => void }> = ({ resume, title, description, handleSubmitForm }) => {
   return (
     <section className="flex flex-col">
+
       <div>
-        <h1>Review Text</h1>
+        <h1 className="text-3xl font-semibold">
+          Review Text
+        </h1>
 
-        <h2>Resume:</h2>
-        <p>{resume}</p>
+        <h2 className="text-2xl text-gray-500 pt-8">
+          Your Old Resume:
+        </h2>
+        <div className="pl-2">
+          <MarkdownBlock text={resume} />
+        </div>
 
-        <h2>Title:</h2>
+        <h2 className="text-2xl text-gray-500 pt-8">
+          Your New Job Title:
+        </h2>
         <p>{title}</p>
 
-        <h2>Description:</h2>
+        <h2 className="text-2xl text-gray-500 pt-8">
+          Your New Job Description:
+        </h2>
         <p>{description}</p>
       </div>
+
       <FormButton onClick={handleSubmitForm} text="Submit" className="float-right"/>
     </section>
   );
