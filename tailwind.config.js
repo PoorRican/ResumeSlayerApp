829eed1f0c6c52dabc49957f151e6b382d7d528b
Josue Figueroa <poor.rican@pm.me>
2023-09-06 04:32:34 -0700

    Fix tailwind config for restructure

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   content: [
     "./src/*.{js,jsx,ts,tsx}",
-    "./src/components/*.{js,jsx,ts,tsx}",
+    "./src/app/**/*.{js,jsx,ts,tsx}",
     './public/index.html',
 
   ],
